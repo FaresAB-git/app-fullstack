fa4574e2aa32fd5ac2ad2949376d683d81008ec3
FaresAB-git <fares.ab74pgmail.com>
2025-02-18 15:22:43 +0100

    login + register + getUser + router

--- client/app-vue/src/router.js
+++ client/app-vue/src/router.js
@@ -2,15 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import homePage from './views/homePage.vue';
 import login from './views/login.vue'; // Import de la nouvelle page
+import register from './views/register.vue';
 
 const routes = [
-  { path: '/', component: homePage },      // Page principale
-  { path: '/login', component: login } // Nouvelle page
+  { path: '/', component: homePage, meta: {requiresAuth: true} },      // Page principale
+  { path: '/login', component: login }, // Nouvelle page
+  { path: '/register', component: register }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token'); // Vérifie si un token est stocké
+
+  if (to.meta.requiresAuth && !token) {
+    next('/login'); // Redirige vers la page de connexion si pas de token
+  } else {
+    next(); // Continue la navigation
+  }
 });
 
 export default router;
---
+++ client/app-vue/src/services/auth.js
@@ -0,0 +1,67 @@
+// src/services/authService.js
+
+export const loginUser = async (email, password) => {
+    const response = await fetch('http://localhost:3000/api/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+  
+    const data = await response.json();
+  
+    if (!response.ok) {
+      throw new Error(data.message || 'Erreur de connexion');
+    }
+  
+    return data;
+  };
+  
+
+
+export const registerUser = async (username, email, password) => {
+  const response = await fetch('http://localhost:3000/api/user/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Erreur de connexion');
+  }
+
+  return data;
+};
+
+
+export const getUser = async () => {
+  
+  const token = localStorage.getItem('token');
+  const response = await fetch('http://localhost:3000/api/user/me', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Erreur de connexion');
+  }
+
+  return data;
+};
